64057eb9ef312d7ce383c03ed6cc9fd0ba022d69
Abi Abdullah <abi.abdullah@bs-23.net>
2020-11-05 13:56:27 +0600

    add label

--- src/app/model/square.ts
+++ src/app/model/square.ts
@@ -7,21 +7,16 @@ export class Square {
   constructor(private ctx: CanvasRenderingContext2D) { }
 
   move(square: Square) {
-
-    this.ctx.clearRect(square.x, square.y, square.z, square.z);
-    this.ctx.fillStyle = this.color;
-    // this.ctx.fillRect(this.x, this.y, this.z, this.z);
-    //  this.ctx.setTransform(1, 0, 0, 1, this.x, this.z * this.y);
-    // this.ctx.strokeText("test", this.x + 5, this.z * this.y + 15);
-    // this.ctx.translate(square.x, this.y);
-    this.ctx.fillRect(this.x, this.y, this.z, this.z);
+    this.ctx.clearRect(square.x - 3, square.y - 3, square.z + 5, square.z + 5);
+    this.ctx.strokeText("test", this.x + 5, this.y + 15);
+    this.ctx.strokeRect(this.x, this.y, this.z, this.z);
   }
 
   draw(left) {
     this.x = (left * this.x) + (this.z * left);
-    this.ctx.fillStyle = this.color;
-    this.ctx.fillRect(this.x, this.z * this.y, this.z, this.z);
-    // this.ctx.strokeText("test", this.x + 5, this.y + 15);
+    // this.ctx.fillStyle = this.color;
+    this.ctx.strokeRect(this.x, this.z * this.y, this.z, this.z);
+    this.ctx.strokeText("test", this.x + 5, this.y + 15);
 
   }
 
